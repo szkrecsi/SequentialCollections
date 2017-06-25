db53b7bfb071c52c958f64a3c496106f05f99b15
szkrecsi <ecsenszki.judit@gmail.com>
2017-06-25 23:01:09 +0200

    OK

---
+++ LookupCollections/LookupCollections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace LookupCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Make the dictionary case insensitive 
+            ListDictionary list = new ListDictionary(new CaseInsensitiveComparer(CultureInfo.InvariantCulture));
+
+            // Add some items
+            list["Estados Unidos"] = "United States of America";
+            list["Canadá"] = "Canada";
+            list["España"] = "Spain";
+
+            // Show the results 
+            Console.WriteLine(list["españa"]);
+            Console.WriteLine(list["CANADÁ"]);
+            Console.ReadKey();
+        }
+    }
+}
