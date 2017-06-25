3410a7b2a80e22768b350a9cf408f6dac909ab8e
szkrecsi <ecsenszki.judit@gmail.com>
2017-06-25 22:59:53 +0200

    OK

---
+++ GenericCollections/GenericCollections/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary <int, string> countryLookup = new Dictionary <int, string>();
+
+            countryLookup[44] = "United Kingdom";
+            countryLookup[33] = "France";
+            countryLookup[31] = "Netherlands";
+            countryLookup[55] = "Brazil";
+
+            Console.WriteLine("The 33 Code is for: {0}", countryLookup[33]);
+
+            foreach (KeyValuePair <int, string> item in countryLookup)
+            {
+                int code = item.Key;
+                string country = item.Value;
+                Console.WriteLine("Code {0} = {1}", code, country);
+            }
+
+            Console.Read();
+        }
+    }
+}
